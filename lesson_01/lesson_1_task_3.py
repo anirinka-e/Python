cb2af597c981f0f60440482d8e4a0604e5e61790
Екатерина <ekarandasova@gmail.com>
2025-06-12 21:51:54 +0300

    Add lesson1

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,9 @@
+first_name = ""
+last_name = ""
+
+print("Введите имя и фамилию: ")
+first_name = input(first_name)
+last_name = input(last_name)
+
+print("Вас зовут: " + first_name + " " + last_name)
+
