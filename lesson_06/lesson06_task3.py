04ed14519cc72fd86fc42588101dc47482f091e0
Екатерина <ekarandasova@gmail.com>
2025-08-07 21:29:03 +0300

    Added lesson_06

---
+++ lesson_06/lesson06_task3.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+
+waiter = WebDriverWait(driver, 40, 0.1)
+waiter.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#text"), "Done!")
+)
+search_img = driver.find_elements(By.CSS_SELECTOR, "img")
+
+print(search_img[3].get_dom_attribute("src"))
+
+driver.quit()
